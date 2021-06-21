a4ecaf7d47972b8ad9fc7180e539b1de9f564bda
NipenKhirsaria <85393984+NipenKhirsaria@users.noreply.github.com>
2021-06-22 02:15:48 +0530

    Update variables_constants.py

--- variables_constants.py
+++ variables_constants.py
@@ -9,13 +9,13 @@ def constants():
     return MU,RE,GRAV_ACC
 
 def variables():
-    m_0 = 2000 #5 # 2000  #1112.4955
-    T = 10*(10**3) #140*(10**-6) # 10*(10**3)
-    Isp = 300 #8500 # 300
-    v_rp_0_BII = np.dot([480+6378,0,0],1000) #np.dot([200+6378,0,0],1000)    # np.dot([480+6378,0,0],1000)   #np.dot([-22141.56177,-3244.5289,0],1000)
-    v_vp_0_BII = np.dot([0,7.7102,0],1000)  #np.dot([0,7.799,0],1000)    #  np.dot([0,7.7102,0],1000)     #[419.39,-2862.034,0]
-    h_p_2 = 16000  # 345  # 16000          # Perigee altitude of final target orbit (km)
-    h_a_2 = 16000  # 345  # 16000          # Apogee altitude of final target orbit (km)
+    m_0 = 2000
+    T = 10*(10**3) 
+    Isp = 300 
+    v_rp_0_BII = np.dot([480+6378,0,0],1000) 
+    v_vp_0_BII = np.dot([0,7.7102,0],1000)  
+    h_p_2 = 16000           # Perigee altitude of final target orbit (km)
+    h_a_2 = 16000           # Apogee altitude of final target orbit (km)
 
 
     return m_0, T, Isp, h_p_2, h_a_2, v_rp_0_BII, v_vp_0_BII
